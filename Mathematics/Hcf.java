4d7d0863a33e7c699c33fab3a4eb9b6706715b82
Muskan Mittal <muskan.mittal@w3villa.com>
2022-11-28 11:48:56 +0530

    code-changed

--- Mathematics/Hcf.java
+++ Mathematics/Hcf.java
@@ -2,20 +2,29 @@ package Mathematics;
 
 public class Hcf {
 
-    int gcd(int a, int b){
-        int res =  Math.min(a, b);
-        while(res  > 0) {
-            if(a % res == 0 && b % res == 0) {
-                 break;
-            }
-            res--;
-        }
-        return res;
-    }
+    // int gcd(int a, int b){
+    //     int res =  Math.min(a, b);
+    //     while(res  > 0) {
+    //         if(a % res == 0 && b % res == 0) {
+    //              break;
+    //         }
+    //         res--;
+    //     }
+    //     return res;
+    // }
+    
     public static void main(String[] args) {
         System.out.println("abc");
-        Hcf obj = new Hcf();
-        System.out.println(obj.gcd(15, 30));
-        System.out.println(obj.gcd(7, 13));
+        // Hcf obj = new Hcf();
+        // System.out.println(obj.gcd(15, 30));
+        // System.out.println(obj.gcd(7, 13));
+
+        int a = 15;
+        int b = 30;
+        for(int i=1; i<=a && i <= b; i++) {
+            if(15 % i == 0 && 30 % i == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
